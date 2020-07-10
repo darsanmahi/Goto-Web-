8963ce4cce98b4fceefa9a31931eca2224f0c41a
developers-headerfile <52238291+developers-headerfile@users.noreply.github.com>
2020-07-10 10:22:28 +0530

    Bug Fixes

--- public/users.js
+++ public/users.js
@@ -18,7 +18,9 @@
     var count1 = 10;
     var count2 = 20;
     var count3 = 30;
-    var flag = 1;
+    var count4 = 40;
+    var count5 = 50;
+    var flag;
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
@@ -27,96 +29,99 @@
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
             var userID = user.uid;
+
+            function forthcome(i) {
+                document.getElementById('forthcomingexist').setAttribute("style", "visibility: shown;");
+                var divider = document.createElement('div');
+                divider.setAttribute("class", "card-body");
+                divider.setAttribute("id", "forthdiv");
+                document.getElementById('forth').appendChild(divider);
+                var h51 = document.createElement('h5');
+                h51.setAttribute("class", "card-header");
+                h51.setAttribute("id", count);
+                document.getElementById('forthdiv').appendChild(h51);
+                document.getElementById(count).innerHTML = i;
+                db.ref('users/' + userID + '/Todo' + '/' + i).on("value", function(snapshot) {
+                    var data1 = snapshot.val();
+                    for (j in data1) {
+                        var h5 = document.createElement('h5');
+                        h5.setAttribute("class", "card-title");
+                        h5.setAttribute("id", count1);
+                        document.getElementById('forthdiv').appendChild(h5);
+                        document.getElementById(count1).innerHTML = j;
+                        db.ref('users/' + userID + '/Todo' + '/' + i + '/' + j).on("value", function(snapshot) {
+                            var data2 = snapshot.val();
+                            var p3 = document.createElement('p');
+                            p3.setAttribute("class", "card-text");
+                            p3.setAttribute("id", count2);
+                            document.getElementById('forthdiv').appendChild(p3);
+                            document.getElementById(count2).innerHTML = data2.Description;
+                        });
+                        count1++;
+                        count2++;
+                    }
+                    count++;
+                });
+
+            }
+
+            function pending(i) {
+                document.getElementById('pendeveexist').setAttribute("style", "visibility: shown;");
+                var divider1 = document.createElement('div');
+                divider1.setAttribute("class", "card-body");
+                divider1.setAttribute("id", "penddiv");
+                document.getElementById('pend').appendChild(divider1);
+                var h511 = document.createElement('h5');
+                h511.setAttribute("class", "card-title");
+                h511.setAttribute("id", count3);
+                document.getElementById('penddiv').appendChild(h511);
+                document.getElementById(count3).innerHTML = i;
+                db.ref('users/' + userID + '/Todo' + '/' + i).on("value", function(snapshot) {
+                    var data1 = snapshot.val();
+                    for (j in data1) {
+                        var h5 = document.createElement('h5');
+                        h5.setAttribute("class", "card-header");
+                        h5.setAttribute("id", count4);
+                        document.getElementById('penddiv').appendChild(h5);
+                        document.getElementById(count4).innerHTML = j;
+                        db.ref('users/' + userID + '/Todo' + '/' + i + '/' + j).on("value", function(snapshot) {
+                            var data2 = snapshot.val();
+                            var p3 = document.createElement('p');
+                            p3.setAttribute("class", "card-text");
+                            p3.setAttribute("id", count5);
+                            document.getElementById('penddiv').appendChild(p3);
+                            document.getElementById(count5).innerHTML = data2.Description;
+                        });
+                        count4++;
+                        count5++;
+                    }
+                    count3++;
+                });
+            }
             db.ref('users/' + userID + '/Todo' + '/').on("value", function(snapshot) {
                 var data = snapshot.val();
                 if (data) {
                     for (i in data) {
                         if (i > today) {
-                            document.getElementById('forthcomingexist').setAttribute("style", "visibility: shown;");
-                            var divider = document.createElement('div');
-                            divider.setAttribute("class", "card-body");
-                            divider.setAttribute("id", "forthdiv");
-                            document.getElementById('forth').appendChild(divider);
-                            var h51 = document.createElement('h5');
-                            h51.setAttribute("class", "card-header");
-                            h51.setAttribute("id", "titforth");
-                            document.getElementById('forthdiv').appendChild(h51);
-                            document.getElementById('titforth').innerHTML = i;
-                            db.ref('users/' + userID + '/Todo' + '/' + i).on("value", function(snapshot) {
-                                var data1 = snapshot.val();
-                                for (j in data1) {
-                                    var h5 = document.createElement('h5');
-                                    h5.setAttribute("class", "card-title");
-                                    h5.setAttribute("id", count);
-                                    document.getElementById('forthdiv').appendChild(h5);
-                                    document.getElementById(count).innerHTML = j;
-                                    db.ref('users/' + userID + '/Todo' + '/' + i + '/' + j).on("value", function(snapshot) {
-                                        var data2 = snapshot.val();
-                                        var p3 = document.createElement('p');
-                                        p3.setAttribute("class", "card-text");
-                                        p3.setAttribute("id", count1);
-                                        document.getElementById('forthdiv').appendChild(p3);
-                                        document.getElementById(count1).innerHTML = data2.Description;
-                                    });
-                                    count++;
-                                    count1++;
-                                }
-                            });
-                            var btn = document.createElement('button');
-                            btn.setAttribute("type", "submit");
-                            btn.setAttribute("class", "btn btn-primary");
-                            btn.setAttribute("id", "deleve");
-                            btn.innerHTML = "Delete Event";
-                            document.getElementById('forthdiv').appendChild(btn);
-                        } else {
-                            var cent = document.createElement("center");
-                            cent.innerHTML = "No Forthcoming Events!";
-                            document.getElementById('forthcomings').setAttribute("style", "visibility: shown;");
-                            document.getElementById('eventdisplay').appendChild(cent);
-
+                            forthcome(i);
                         }
                         if (i <= today) {
-                            var divider1 = document.createElement('div');
-                            divider1.setAttribute("class", "card-body");
-                            divider1.setAttribute("id", "penddiv");
-                            document.getElementById('pend').appendChild(divider1);
-                            var h511 = document.createElement('h5');
-                            h511.setAttribute("class", "card-title");
-                            h511.setAttribute("id", "titpend");
-                            document.getElementById('penddiv').appendChild(h511);
-                            document.getElementById('titpend').innerHTML = i;
-                            db.ref('users/' + userID + '/Todo' + '/' + i).on("value", function(snapshot) {
-                                var data1 = snapshot.val();
-                                for (j in data1) {
-                                    var h5 = document.createElement('h5');
-                                    h5.setAttribute("class", "card-header");
-                                    h5.setAttribute("id", count2);
-                                    document.getElementById('penddiv').appendChild(h5);
-                                    document.getElementById(count2).innerHTML = j;
-                                    db.ref('users/' + userID + '/Todo' + '/' + i + '/' + j).on("value", function(snapshot) {
-                                        var data2 = snapshot.val();
-                                        var p3 = document.createElement('p');
-                                        p3.setAttribute("class", "card-text");
-                                        p3.setAttribute("id", count3);
-                                        document.getElementById('penddiv').appendChild(p3);
-                                        document.getElementById(count3).innerHTML = data2.Description;
-                                    });
-                                    count++;
-                                    count1++;
-                                }
-                                var btn = document.createElement('button');
-                                btn.setAttribute("type", "submit");
-                                btn.setAttribute("class", "btn btn-danger");
-                                btn.setAttribute("id", "deleve");
-                                btn.innerHTML = "Delete Event";
-                                document.getElementById('penddiv').appendChild(btn);
-                            });
-                        } else {
-                            var nopend = document.createElement('center');
-                            nopend.innerText = "No Pending Events!";
-                            document.getElementById('pending').setAttribute("style", "visibility: shown;");
-                            document.getElementById('nopendeve').appendChild(nopend);
+                            pending(i);
                         }
+                    }
+                    var e1 = document.getElementById('forthcomingexist');
+                    if (e1.style.visibility == 'hidden') {
+                        var cen1 = document.createElement("center");
+                        cen1.innerHTML = "No Forthcoming Events!";
+                        document.getElementById('forthcomings').setAttribute("style", "visibility: shown;");
+                        document.getElementById('eventdisplay').appendChild(cen1);
+                    }
+                    var e2 = document.getElementById('pendeveexist');
+                    if (e2.style.visibility == 'hidden') {
+                        var nopend2 = document.createElement('center');
+                        nopend2.innerText = "No Pending Events!";
+                        document.getElementById('pending').setAttribute("style", "visibility: shown;");
+                        document.getElementById('nopendeve').appendChild(nopend2);
                     }
                 } else {
                     var cen = document.createElement("center");
@@ -136,28 +141,82 @@
                             var data3 = snapshot.val();
                             console.log(data3);
                             for (i in data3) {
-                                db.ref('users/' + userID + '/Todo' + i).on("value", function(snapshot) {
+                                db.ref('users/' + userID + '/Todo' + '/' + i).on("value", function(snapshot) {
                                     var data4 = snapshot.val();
                                     console.log(data4);
                                     for (j in data4) {
-                                        console.log('J' + j);
                                         if (j != name) {
+                                            console.log(j, name);
                                             flag = 0;
                                         } else {
+                                            console.log(j, name);
                                             flag = 1;
+                                            var names = i;
+                                            window.names = names;
+                                            break;
+                                        }
+                                        if (flag == 1) {
+                                            console.log(flag);
+                                            break;
                                         }
                                     }
                                 });
-                                if (flag == 1) {
-                                    db.ref('users/' + userID + '/Todo' + '/' + i + '/' + name).remove().then(function() {
-                                        alert("Event deleted successfully");
-                                        //window.location = "users.html";
-                                    }).catch(function(error) {
-                                        alert(error.message);
-                                    });
-                                } else {
-                                    alert("No Such Event Exist!");
-                                }
+                            }
+                            var names = window.names;
+                            console.log(names);
+                            if (names) {
+                                db.ref('users/' + userID + '/Todo' + '/' + names + '/' + name).remove().then(function() {
+                                    window.location = "users.html";
+                                    alert("Event deleted successfully");
+                                }).catch(function(error) {
+                                    alert(error.message);
+                                });
+                            } else {
+                                alert("No Such Event Exist!");
+                            }
+                        });
+                    });
+                }
+                if (document.getElementById('deleve1')) {
+                    const deleve1 = document.getElementById('deleve1');
+                    deleve1.addEventListener("click", function() {
+                        var name = prompt("Enter the name of the event");
+                        db.ref('users/' + userID + '/Todo').on("value", function(snapshot) {
+                            var data3 = snapshot.val();
+                            console.log(data3);
+                            for (i in data3) {
+                                db.ref('users/' + userID + '/Todo' + '/' + i).on("value", function(snapshot) {
+                                    var data4 = snapshot.val();
+                                    console.log(data4);
+                                    for (j in data4) {
+                                        if (j != name) {
+                                            console.log(j, name);
+                                            flag = 0;
+                                        } else {
+                                            console.log(j, name);
+                                            flag = 1;
+                                            var names = i;
+                                            window.names = names;
+                                            break;
+                                        }
+                                        if (flag == 1) {
+                                            console.log(flag);
+                                            break;
+                                        }
+                                    }
+                                });
+                            }
+                            var names = window.names;
+                            console.log(names);
+                            if (names) {
+                                db.ref('users/' + userID + '/Todo' + '/' + names + '/' + name).remove().then(function() {
+                                    window.location = "users.html";
+                                    alert("Event deleted successfully");
+                                }).catch(function(error) {
+                                    alert(error.message);
+                                });
+                            } else {
+                                alert("No Such Event Exist!");
                             }
                         });
                     });
